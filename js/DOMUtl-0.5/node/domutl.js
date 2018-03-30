070ce0bce3931a3e372fa8a82bd959b95979e336
MICKeyzwo <singing.programmer@gmail.com>
2018-03-31 06:07:37 +0900

    debug ver 0.4 and add ver 0.5

--- js/DOMUtl-0.5/node/domutl.js
+++ js/DOMUtl-0.5/node/domutl.js
@@ -1,37 +1,31 @@
 module.exports = {
     el: (key, ...args) => {
         key = (key || "").toString();
-        let res = [[], [], []],
-            begin = 0, mode = 0;
-        key.split("").forEach((s, c) => {
-            if (s == "#" || s == ".") {
-                res[mode].push(key.substring(begin, c));
-                begin = c + 1;
-                mode = s == "#" ? 1 : 2;
-            }
-        });
-        res[mode].push(key.substring(begin, key.length));
-        let e = document.createElement(res[0][0] || "div");
-        if (res[1].length > 0) e.id = res[1][0];
-        if (res[2].length > 0) e.className = res[2].join(" ");
+        let tag = key.match(/^[a-z]+[0-9]/);
+        tag = tag ? tag[0] : "div";
+        let e = document.createElement(tag);
+        let id = key.match(/#[a-z|0-9]+/);
+        if (id) e.id = id[0].replace("#", "");
+        let cls = key.match(/\.[a-z|0-9]+/g);
+        if (cls) e.className = cls.map(s => s.replace(".", "")).join(" ");
         args.forEach(item => {
             if (typeof item == "object") {
                 if (item instanceof Node) {
                     e.appendChild(item);
                 } else {
-                    for (var p in item) {
+                    for (let p in item) {
                         if (p === "style") {
                             if (typeof item[p] == "string") {
                                 e.style = item[p];
                             } else if (typeof item[p] == "object") {
-                                var rule = "";
-                                for (var r in item[p]) {
+                                let rule = "";
+                                for (let r in item[p]) {
                                     rule += r + ":" + item[p][r] + ";";
                                 }
                                 e.style = rule;
                             }
                         } else if (p === "on" && typeof item[p] == "object") {
-                            for (var o in item[p]) {
+                            for (let o in item[p]) {
                                 e.addEventListener(o, item[p][o]);
                             }
                         } else {
@@ -40,7 +34,7 @@ module.exports = {
                     }
                 }
             } else if (typeof item == "string") {
-                e.innerHTML += item;
+                e.textContent += item;
             } else if (typeof item == "function") {
                 item(e);
             }
@@ -61,4 +55,4 @@ module.exports = {
     after: (target, elm) => target.parentNode.insertBefore(elm, target.nextSibling),
     getel: (key, root = document) => root.querySelector(key),
     getels: (key, root = document) => root.querySelectorAll(key)
-};
+}
